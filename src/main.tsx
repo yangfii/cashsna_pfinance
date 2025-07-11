1596fc483c33f067be391817cb31c6a29623d833
PRES YANG <ismileyang@gmail.com>
2025-07-11 14:13:03 +0000

    add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights(); // inject BEFORE render, also fine
 
 createRoot(document.getElementById("root")!).render(<App />);
+
